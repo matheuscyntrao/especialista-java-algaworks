ce08fec2e0deb99607cb28e2cb0a86ed27c9bc17
Matheus Cyntrão de Souza <matheus.cyntrao@gmail.com>
2024-09-18 20:05:49 -0300

    Palavras reservadas do java

--- OlaMergulhador.java
+++ OlaMergulhador.java
@@ -13,6 +13,64 @@ public class OlaMergulhador {
     // Acessar o java code conventions e google style guide
     // Principalmente da oracle
 
+    /*
+
+     Palavras reservadas do java:
+
+     abstract
+     class
+     extends
+     implements
+     new
+     static
+     throws
+     enum
+     boolean
+     const = não possui significado no compilador
+     final
+     import
+     package
+     strictfp
+     transient
+     break
+     continue
+     finally
+     instanceof
+     private
+     super
+     try
+     byte
+     default
+     float
+     int
+     protected
+     switch
+     void
+     case
+     do
+     for
+     interface
+     public
+     synchronize
+     volatile
+     catch
+     double
+     goto = não possui significado no compilador
+     long
+     return
+     this
+     while
+     chat
+     else
+     if
+     native
+     short
+     throw
+     assert
+
+     */
+
+
     public static void main(String[] args) {
         System.out.println("Olá mergulhador!");
     }
