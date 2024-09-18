b47bda4eacbcfe98ba32bb1590f16952aeea16db
Matheus Cyntrão de Souza <matheus.cyntrao@gmail.com>
2024-09-18 19:58:06 -0300

    Adicionando comentários ao código

--- OlaMergulhador.java
+++ OlaMergulhador.java
@@ -1,5 +1,14 @@
 public class OlaMergulhador {
 
+
+    // Comentários
+
+    /*
+        COmentários em bloco
+     */
+
+    //TODO: keyword reservado para novas features/funcionalidades
+
     public static void main(String[] args) {
         System.out.println("Olá mergulhador!");
     }
